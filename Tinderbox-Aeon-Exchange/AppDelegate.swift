aa64286bc994cbe27bbcadcb13226ed06d190380
Michael Prenez-Isbell <mprenez-isbell@ML-C02MJ1D2FD59.utg.uvn.net>
2015-06-24 11:37:13 -0400

    Initial Commit

---
+++ Tinderbox-Aeon-Exchange/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tinderbox-Aeon-Exchange
+//
+//  Created by Michael Prenez-Isbell on 6/24/15.
+//  Copyright (c) 2015 Michael Prenez-Isbell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tinderbox-Aeon-Exchange/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tinderbox-Aeon-Exchange
+//
+//  Created by Michael Prenez-Isbell on 6/24/15.
+//  Copyright (c) 2015 Michael Prenez-Isbell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
