c36e9a93930c367763fffe15021ada8f93e5e588
Michael Prenez-Isbell <michael.isbell@gmail.com>
2015-06-26 08:00:15 -0400

    added first aexml code

--- Tinderbox-Aeon-Exchange/ViewController.swift
+++ Tinderbox-Aeon-Exchange/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: NSViewController,  NSXMLParserDelegate {
     var parseTbx : Bool?
     var parseAeon : Bool?
     
-
+    
     @IBOutlet var textViewTinderbox: TASCTextViewTB!
     @IBOutlet var textViewAeon: TASCTextViewAE!
     
@@ -46,43 +46,68 @@ class ViewController: NSViewController,  NSXMLParserDelegate {
         let _ = openPanel.URLs
         let _ = openPanel.runModal()
         
-        if let _  = openPanel.URL {
-            print("file found")
+        if let chosenfile  = openPanel.URL {
+            println("file found")
             parseAeon = true
             
-          //  textViewAeon.string = "hello, world"
-
+            //  textViewAeon.string = "hello, world"
+            
             let xmlParser = NSXMLParser(contentsOfURL: openPanel.URL!)
             xmlParser!.delegate = self
             xmlParser!.parse()
             parseAeon = false
             
+            var error: NSError?
+            
+            //let text2 = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+            var path = chosenfile.path!
+            var data: NSData = NSFileManager.defaultManager().contentsAtPath(path)!
+            var txt: NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+            
+            if let xmlDoc = AEXMLDocument(xmlData: data, error: &error) {
+                
+                for child in xmlDoc.root.children {
+                    println(child.name)
+                    for cchold in child.children {
+                        println(cchold.name)
+                        for ccchold in cchold.children {
+                            println(ccchold.name)
+                        }
+                    }
+                }
+                
+            }
+            
         }
         
         // retry the completion handler version
-     
+        
         // aeonxml
         //test
         
     }
     
-    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String]) {
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
+        
+        
+        
+        //    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String]) {
         //print("Element's name is \(elementName)")
         //print("Element's attributes are \(attributeDict)")
         
         if parseAeon! {
-            print("parseaeon")
+            //print("parseaeon")
             let str = "Element's name is \(elementName) \n Element's attributes are \(attributeDict) \n"
             textViewAeon.textStorage?.mutableString.appendString(str)
         }
         if parseTbx! {
-            print("parsetinderbox")
+            // print("parsetinderbox")
             let str = "Element's name is \(elementName) \n Element's attributes are \(attributeDict) \n"
             textViewTinderbox.textStorage?.mutableString.appendString(str)
         }
     }
-
-
+    
+    
     
     
     @IBAction func loadTinderboxXmlDocument(sender: AnyObject) {
@@ -102,33 +127,61 @@ class ViewController: NSViewController,  NSXMLParserDelegate {
         let _ = openPanel.URLs
         let _ = openPanel.runModal()
         
-        if let _  = openPanel.URL {
+        if let chosenfile  = openPanel.URL {
             print("file found")
             parseTbx = true
+            
+            //  textViewAeon.string = "hello, world"
             
             let xmlParser = NSXMLParser(contentsOfURL: openPanel.URL!)
             xmlParser!.delegate = self
             xmlParser!.parse()
+            parseAeon = false
+            
+            var error: NSError?
+            
+            //let text2 = String(contentsOfFile: path, encoding: NSUTF8StringEncoding, error: nil)
+            var path = chosenfile.path!
+            var data: NSData = NSFileManager.defaultManager().contentsAtPath(path)!
+            var txt: NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+            
+            if let xmlDoc = AEXMLDocument(xmlData: data, error: &error) {
+                
+                for child in xmlDoc.root.children {
+                    println(child.name)
+                    for cchold in child.children {
+                        println(cchold.name)
+                        for ccchold in cchold.children {
+                            println(ccchold.name)
+                        }
+                    }
+                }
+                
+            }
+            
+            //   let xmlParser = NSXMLParser(contentsOfURL: openPanel.URL!)
+            //   xmlParser!.delegate = self
+            //   xmlParser!.parse()
             parseTbx = false
             
-        
+            
         }
         
     }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
