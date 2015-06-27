9928e3f2ad21ff9429df1c06a4a26b473525c716
Michael Prenez-Isbell <michael.isbell@gmail.com>
2015-06-27 11:19:50 -0400

    added person class to walk through outline view tutorial, since i haven't created one in swift yet.

---
+++ Tinderbox-Aeon-Exchange/OutlineViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OutlineViewController.swift
+//  Tinderbox-Aeon-Exchange
+//
+//  Created by prenez on 6/27/15.
+//  Copyright © 2015 Michael Prenez-Isbell. All rights reserved.
+//
+
+import Foundation
