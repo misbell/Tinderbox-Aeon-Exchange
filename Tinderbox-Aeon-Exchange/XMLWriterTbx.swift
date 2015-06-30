f3319e0f752a92ca10ff88fda380c6710a16910c
Michael Prenez-Isbell <michael.isbell@gmail.com>
2015-06-30 13:53:49 -0400

    added more aeon group attributes, attrib types, notes

--- Tinderbox-Aeon-Exchange/XMLWriterTbx.swift
+++ Tinderbox-Aeon-Exchange/XMLWriterTbx.swift
@@ -21,6 +21,51 @@ class XMLWriterTbx  {
     
     func writeTinderboxXMLDocument() {
         
+/* <attrib Name="IsPrototype" 
+parent="General" 
+editable="1" 
+visibleInEditor="1" 
+type="4" 
+canInherit="0" 
+default="false">
+*/
+        
+        
+/*
+0 - string
+1 - color
+2 - number
+3 - file
+4 - boolean
+5 - date
+6 - (alias)
+7 - action
+8 - set
+9 - url
+10 - list
+*/
+        
+        let aeonAttributes = ["AeonEventNoteStatus",
+        "AeonEventID",
+        "AeonEventLocked",
+        "AeonEventTitle",
+        "AeonEventAllDay",
+        "AeonEventStartDate",
+        "AeonEventDescription",
+        "AeonEventIncludeInExport",
+        "AeonEventLabel",
+        "AeonEventArc",
+        "AeonEventRelationships",
+        "AeonEventTags",
+        "AeonEventCompleted",
+        "AeonEventDuration",
+        "AeonEventDurationUnit",
+        "AeonEventShowTime",
+        "AeonEventShowDay",
+        "AeonEventShowMonth",
+        "AeonEventExternalLinks",
+        "AeonEventNoteStatus" ]
+        
         print("one  \(self.tbxItem)")
         print("many \(self.tbxItems)")
         
@@ -35,18 +80,57 @@ class XMLWriterTbx  {
         }
         
         
-        //let attributes = [
-        let rootAttribElement = AEXMLElement("Tools")
+        
+        let rootAttribElement = AEXMLElement("Tools") // like system and user
         rootAttribElement.value = ""
-        rootAttribElement.addAttribute("parent", value: "anything")
         rootAttribElement.addAttribute("editable", value: "0")
         rootAttribElement.addAttribute("visibleInEditor", value: 1)
         rootAttribElement.addAttribute("default", value: "")
 
+        let groupAttribElement = AEXMLElement("AeonTimeline")
+        groupAttribElement.addAttribute("parent", value: "Tools")
+        groupAttribElement.addAttribute("editable", value: "0")
+        groupAttribElement.addAttribute("visibleInEditor", value: 1)
+        groupAttribElement.addAttribute("default", value: "")
         
+        let AEEventAttribElement = AEXMLElement("AeonTimelineEvent")
+        AEEventAttribElement.addAttribute("parent", value: "AeonTimeline")
+        AEEventAttribElement.addAttribute("editable", value: "0")
+        AEEventAttribElement.addAttribute("visibleInEditor", value: 1)
+        AEEventAttribElement.addAttribute("default", value: "")
+        
+        
+        addAeonTimelineEventAttributes(AEEventAttribElement)
+        
+        
+        let AEArcAttribElement = AEXMLElement("AeonTimelineArc")
+        AEArcAttribElement.addAttribute("parent", value: "AeonTimeline")
+        AEArcAttribElement.addAttribute("editable", value: "0")
+        AEArcAttribElement.addAttribute("visibleInEditor", value: 1)
+        AEArcAttribElement.addAttribute("default", value: "")
+        
+        let AEEntityAttribElement = AEXMLElement("AeonTimelineEntity")
+        AEEntityAttribElement.addAttribute("parent", value: "AeonTimeline")
+        AEEntityAttribElement.addAttribute("editable", value: "0")
+        AEEntityAttribElement.addAttribute("visibleInEditor", value: 1)
+        AEEntityAttribElement.addAttribute("default", value: "")
         
         
     }
+    
+    func addAeonTimelineEventAttributes(AEEventAttribElement : AEXMLElement) {
+        
+    }
+    
+    func addAeonTimelineArcAttributes(AEArcAttribElement : AEXMLElement) {
+        
+    }
+    
+    
+    func addAeonTimelineEntityAttributes(AEEntityAttribElement : AEXMLElement) {
+        
+    }
+    
     
     func writeChildren(item: TbxItem) {
         
